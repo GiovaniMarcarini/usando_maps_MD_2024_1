54599fb785d8f9e3670fc2f2fd671743c713539b
giovani <inf_giovani@hotmail.com>
2024-05-28 21:17:08 -0300

    Mapa Interno completo

--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapasPage extends StatefulWidget{
@@ -20,8 +21,24 @@ class MapasPage extends StatefulWidget{
 class _MapasPageState extends State<MapasPage>{
   final _controller = Completer<GoogleMapController>();
 
+  StreamSubscription<Position?>? _subscription;
+
+  @override
+  void initState(){
+    super.initState();
+    _monitorarLocalizacao();
+  }
+
+  @override
+  void close(){
+    _subscription?.cancel();
+    _subscription = null;
+    super.dispose();
+  }
+
   Widget build(BuildContext context){
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primaryContainer,
       appBar: AppBar(title: const Text('Mapa Interno'),),
       body: GoogleMap(
         mapType: MapType.normal,
@@ -30,9 +47,9 @@ class _MapasPageState extends State<MapasPage>{
             markerId: MarkerId('1'),
             position: LatLng(widget.latitude, widget.longitude),
             infoWindow: const InfoWindow(
-              title: 'Utfpr',
-            )
-          )
+              title: 'Unimater',
+            ),
+          ),
         },
         initialCameraPosition: CameraPosition(
           target: LatLng(widget.latitude, widget.longitude),
@@ -44,6 +61,23 @@ class _MapasPageState extends State<MapasPage>{
         myLocationEnabled: true,
       ),
     );
+  }
+
+  void _monitorarLocalizacao(){
+    final LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 100,
+    );
+    _subscription = Geolocator.getPositionStream(
+        locationSettings: locationSettings).listen((Position position) async {
+          final controller  = await _controller.future;
+          final zoom = await controller.getZoomLevel();
+          controller.animateCamera(CameraUpdate.newCameraPosition(
+              CameraPosition(
+                  target: LatLng(position.latitude, position.longitude),
+                zoom: zoom,
+              )));
+    });
   }
 
 }
